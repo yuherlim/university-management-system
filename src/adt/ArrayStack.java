fcaa4c2322e47ff57da6da590c3c82b00e06b715
yuherlim <110385648+yuherlim@users.noreply.github.com>
2023-08-25 19:19:17 +0800

    - Add arrayStack, arrayQueue, QueueInterface, StackInterface - Update ListInterface with new methods - Added stub for the implementations of the abstract method in ArrayList

---
+++ src/adt/ArrayStack.java
@@ -0,0 +1,69 @@
+package adt;
+
+/**
+ * ArrayStack.java A class that implements the ADT Stack using an array.
+ *
+ * @author Frank M. Carrano
+ * @version 2.0
+ * @param <T>
+ */
+public class ArrayStack<T> implements StackInterface<T> {
+
+  private T[] array;
+  private int topIndex; // index of top entry
+  private static final int DEFAULT_CAPACITY = 50;
+
+  public ArrayStack() {
+    this(DEFAULT_CAPACITY);
+  }
+
+  public ArrayStack(int initialCapacity) {
+    array = (T[]) new Object[initialCapacity];
+    topIndex = -1;
+  }
+
+  @Override
+  public void push(T newEntry) {
+    topIndex++;
+
+    if (topIndex < array.length) {
+      array[topIndex] = newEntry;
+    }
+  }
+
+  @Override
+  public T peek() {
+    T top = null;
+
+    if (!isEmpty()) {
+      top = array[topIndex];
+    }
+
+    return top;
+  } 
+
+  @Override
+  public T pop() {
+    T top = null;
+     System.out.println(top);
+    if (!isEmpty()) {
+      top = array[topIndex];
+      array[topIndex] = null;
+      topIndex--;
+      
+     
+    } // end if
+    
+    return top;
+  } 
+
+  @Override
+  public boolean isEmpty() {
+    return topIndex < 0;
+  } 
+
+  public void clear() {
+    topIndex = -1;
+  } 
+  
+} 
---
+++ src/adt/DoublyLinkedList.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adt;
+
+/**
+ *
+ * @author Yu
+ */
+public class DoublyLinkedList<T> implements ListInterface<T> {
+
+    @Override
+    public boolean add(T newEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(int newPosition, T newEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean addToStart(T newEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T remove(int givenPosition) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T remove(T anEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T removeFirst() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T removeLast() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean replace(int givenPosition, T newEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean replaceFirst(T newEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean replaceLast(T newEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T getEntry(int givenPosition) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T getFirst() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T getLast() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean contains(T anEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getNumberOfEntries() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean isFull() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
