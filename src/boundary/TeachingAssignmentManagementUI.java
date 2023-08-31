f341b2a468893e23d6d1fc7bf58556c4ff3a8ebb
ong <ong5828@gmail.com>
2023-08-31 18:01:26 +0800

    Progress

--- src/boundary/TeachingAssignmentManagementUI.java
+++ src/boundary/TeachingAssignmentManagementUI.java
@@ -4,10 +4,71 @@
  */
 package boundary;
 
+import control.TutorInputValidator;
+import control.CourseInputValidator;
+import java.util.*;
+import utility.MessageUI;
+
 /**
  *
  * @author ong58
  */
 public class TeachingAssignmentManagementUI {
+
+    Scanner scanner = new Scanner(System.in);
+    private TutorInputValidator tutorInputValidator = new TutorInputValidator();
+    private CourseInputValidator courseInputValidator = new CourseInputValidator();
+
+    public int getMenuChoice() {
+        boolean valid = false;
+        int selection = -1;
+        while (true) {
+            try {
+                System.out.println("\nMAIN MENU");
+                System.out.println("1. Assign Tutor");
+                System.out.println("2. Modify Assigned Tutor");
+                System.out.println("3. Find Teaching Assignment");
+                System.out.println("4. List Teaching Assignent");
+                System.out.println("5. Generate Report");
+                System.out.println("0. Quit");
+                System.out.print("Enter choice: ");
+                selection = scanner.nextInt();
+                if (selection >= 0 && selection <= 4) {
+                    break;
+                }
+                MessageUI.displayInvalidChoiceMessage();
+            } catch (Exception e) {
+                System.out.println("Please enter a valid selection in integer");
+                scanner.nextLine();
+                
+            }
+        } 
+        return selection;
+    }
     
+      public int getAssigmTutorOption() {
+        boolean valid = false;
+        int selection = -1;
+        while (true) {
+            try {
+                System.out.println("\nAssign Tutor menu");
+                System.out.println("1. Assign Tutor by course");
+                System.out.println("2. Assign Tutor by Available slot");
+                System.out.println("0. Quit");
+                System.out.print("Enter choice: ");
+                selection = scanner.nextInt();
+                if (selection >= 0 && selection <= 2) {
+                    break;
+                }
+                MessageUI.displayInvalidChoiceMessage();
+            } catch (Exception e) {
+                System.out.println("Please enter a valid selection in integer");
+                scanner.nextLine();
+            }
+        } 
+        return selection;
+    }
+    
+    
+
 }
--- src/control/TeachingAssignmentManagement.java
+++ src/control/TeachingAssignmentManagement.java
@@ -4,7 +4,15 @@
  */
 package control;
 
+import adt.ArrayList;
+import adt.CircularDoublyLinkedList;
+import adt.ListInterface;
+import boundary.TeachingAssignmentManagementUI;
+import dao.TeachingAssignmentDAO;
+import entity.Course;
+import entity.TeachingAssignment;
 import entity.Tutor;
+import java.util.Scanner;
 
 /**
  *
@@ -12,8 +20,45 @@ import entity.Tutor;
  */
 public class TeachingAssignmentManagement {
 
-    public void assignTutor() {
+    private ListInterface<TeachingAssignment> teachingAssignmentList = new CircularDoublyLinkedList<>();
+    private static TeachingAssignmentDAO teachingAssignmentDAO = new TeachingAssignmentDAO();
+    private static TeachingAssignmentManagementUI teachingAssignmentUI = new TeachingAssignmentManagementUI();
+    Scanner scan = new Scanner(System.in);
+
+    public TeachingAssignmentManagement() {
+        teachingAssignmentList = teachingAssignmentDAO.retrieveFromFile();
     }
+
+    public void assignTutor(){
+        
+        int selection = -1;
+       do {
+            selection = teachingAssignmentUI.getMenuChoice();
+            switch (selection) {
+                case 1:
+                    assignByCourse();
+                    break;
+                case 2:
+                    assignBySlot();
+                    break;
+                default:
+            }
+        } while (selection != 0);       
+    }
+    
+    public void assignByCourse(){
+        
+    
+    
+    }
+    
+    public void assignBySlot(){
+    
+    
+    
+    }
+    
+    public void Tutgrp(){}
 
     public void modifyTutor() {
     }
@@ -23,9 +68,50 @@ public class TeachingAssignmentManagement {
 
     public void replaceTutor() {
     }
-    
-    public void displayTeachingAssignment(){}
-    
 
+    public void findTeachingAssignemt() {
+    }
+
+    public ArrayList<TeachingAssignment> searchTeachingAssignmentByTutor(Tutor tutorSearch) {
+        return null;
+    }
+
+    public ArrayList<TeachingAssignment> searchTeachingAssignmentByCourse(Course courseSearch) {
+        return null;
+    }
+
+    public void ListTeachingAssignment() {
+    }
+
+    public void generateTeachingAssignmentReport() {
+    }
+
+    public static void main(String[] args) {
+        TeachingAssignmentManagement teachingAssignmentList = new TeachingAssignmentManagement();
+        int selection = -1;
+
+        do {
+            selection = teachingAssignmentList.teachingAssignmentUI.getMenuChoice();
+            switch (selection) {
+                case 1:
+                    teachingAssignmentList.assignTutor();
+                    break;
+                case 2:
+                    teachingAssignmentList.modifyTutor();
+                    break;
+                case 3:
+                    teachingAssignmentList.findTeachingAssignemt();
+                    break;
+                case 4:
+                    teachingAssignmentList.ListTeachingAssignment();
+                    break;
+                case 5:
+                    teachingAssignmentList.generateTeachingAssignmentReport();
+                    break;
+                default:
+            }
+        } while (selection != 0);
+
+    }
 
 }
